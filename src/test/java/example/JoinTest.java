be380f040421e0fb38f221a4beaf3adbf7df6f23
Ivan Greene <ivan@ivan.sh>
2020-09-03 13:46:30 -0500

    Upgrade libraries

--- src/test/java/example/JoinTest.java
+++ src/test/java/example/JoinTest.java
@@ -1,45 +1,41 @@
 package example;
 
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.neo4j.driver.v1.Config;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.harness.ServerControls;
-import org.neo4j.harness.TestServerBuilders;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.driver.Config;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
+
 public class JoinTest {
 
-    private static final Config driverConfig = Config.build().withoutEncryption().toConfig();
-    private ServerControls embeddedDatabaseServer;
+    private static final Config driverConfig = Config.builder().withoutEncryption().build();
+    private Neo4j embeddedDatabaseServer;
 
-    @BeforeAll
-    void initializeNeo4j() {
-
-        this.embeddedDatabaseServer = TestServerBuilders
+    @Before
+    public void initializeNeo4j() {
+        this.embeddedDatabaseServer = Neo4jBuilders
                 .newInProcessBuilder()
                 .withFunction(Join.class)
-                .newServer();
+                .build();
     }
 
     @Test
     public void shouldAllowIndexingAndFindingANode() {
 
         // This is in a try-block, to make sure we close the driver after the test
-        try( Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
+        try (Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
              Session session = driver.session()) {
 
             // When
-            String result = session.run( "RETURN example.join(['Hello', 'World']) AS result").single().get("result").asString();
+            String result = session.run("RETURN example.join(['Hello', 'World']) AS result").single().get("result").asString();
 
             // Then
-            assertThat( result).isEqualTo(( "Hello,World" ));
+            assertThat(result).isEqualTo(("Hello,World"));
         }
     }
 }
--- src/test/java/example/LastTest.java
+++ src/test/java/example/LastTest.java
@@ -1,45 +1,41 @@
 package example;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
-import org.neo4j.driver.v1.Config;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.harness.ServerControls;
-import org.neo4j.harness.TestServerBuilders;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+import org.junit.Before;
+import org.junit.Test;
+import org.neo4j.driver.Config;
+import org.neo4j.driver.Driver;
+import org.neo4j.driver.GraphDatabase;
+import org.neo4j.driver.Session;
+import org.neo4j.harness.Neo4j;
+import org.neo4j.harness.Neo4jBuilders;
+
 public class LastTest {
 
-    private static final Config driverConfig = Config.build().withoutEncryption().toConfig();
-    private ServerControls embeddedDatabaseServer;
+    private static final Config driverConfig = Config.builder().withoutEncryption().build();
+    private Neo4j embeddedDatabaseServer;
 
-    @BeforeAll
-    void initializeNeo4j() {
-
-        this.embeddedDatabaseServer = TestServerBuilders
+    @Before
+    public void initializeNeo4j() {
+        this.embeddedDatabaseServer = Neo4jBuilders
                 .newInProcessBuilder()
                 .withAggregationFunction(Last.class)
-                .newServer();
+                .build();
     }
-
 
     @Test
     public void shouldAllowReturningTheLastValue() {
 
         // This is in a try-block, to make sure we close the driver after the test
-        try( Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
+        try (Driver driver = GraphDatabase.driver(embeddedDatabaseServer.boltURI(), driverConfig);
              Session session = driver.session()) {
 
             // When
-            Long result = session.run( "UNWIND range(1,10) as value RETURN example.last(value) AS last").single().get("last").asLong();
+            Long result = session.run("UNWIND range(1,10) as value RETURN example.last(value) AS last").single().get("last").asLong();
 
             // Then
-            assertThat(result).isEqualTo( 10L );
+            assertThat(result).isEqualTo(10L);
         }
     }
 }
